b8aa8e5c66bc72889cff309ffe87a8a60ce81dad
mohamed-elbermawy <mohamed77bermawy@gmail.com>
2021-12-20 02:16:08 +0200

    adding Slider Component and animations on it

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,10 +9,11 @@ import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { HomeComponent } from './pages/home/home.component';
 import { MoviesComponent } from './pages/movies/movies.component';
+import { SliderComponent } from './components/slider/slider.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, FooterComponent, HomeComponent, MoviesComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  declarations: [AppComponent, HeaderComponent, FooterComponent, HomeComponent, MoviesComponent, SliderComponent],
+  imports: [BrowserModule,BrowserAnimationsModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ src/app/components/slider/slider.component.ts
@@ -0,0 +1,28 @@
+import { animate, state, style, transition, trigger } from '@angular/animations';
+import { Component, Input, OnInit } from '@angular/core';
+import { Movie } from './../../models/movie';
+
+@Component({
+  selector: 'app-slider',
+  templateUrl: './slider.component.html',
+  styleUrls: ['./slider.component.scss'],
+  animations:[
+    trigger('sliderFade',[
+      state('void',style({opacity:0})),
+      transition('void <=>*',[animate('1s')])
+    ])
+  ]
+})
+export class SliderComponent implements OnInit {
+
+  @Input() items: Movie[] = [];
+  currentIndex: number = 0;
+
+  constructor() {}
+
+  ngOnInit(): void {
+    setInterval(()=>{
+      this.currentIndex = ++this.currentIndex % this.items.length;
+    },5000);
+  }
+}
